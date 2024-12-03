ce028fd243cb18fdfcc4b21e647de8d4e27deb8c
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-03 10:14:48 -0500

    useMemo

--- src/components/T16useCallbackIndex.jsx
+++ src/components/T16useCallbackIndex.jsx
@@ -1,6 +1,14 @@
 import { useState, useCallback } from "react";
 import Todos from "./T16useCallbackTodos";
 
+// The React useCallback Hook returns a memoized callback function.
+// Think of memoization as caching a value so that it does not need to be recalculated.
+// The useCallback Hook only runs when one of its dependencies update.
+// This can improve performance.
+// The useCallback and useMemo Hooks are similar. 
+// The main difference is that useMemo returns a memoized value 
+// and useCallback returns a memoized function. 
+
 export default function T16useCallbackIndex () {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
@@ -8,6 +16,8 @@ export default function T16useCallbackIndex () {
   const increment = () => {
     setCount((c) => c + 1);
   };
+
+  // Use the useCallback Hook to prevent the Todos component from re-rendering needlessly
   const addTodo = useCallback(() => {
     setTodos((t) => [...t, "New Todo"]);
   }, [todos]);
@@ -23,3 +33,10 @@ export default function T16useCallbackIndex () {
     </>
   );
 };
+
+// You will notice that the Todos component re-renders even when the todos do not change.
+// Why does this not work? We are using memo, 
+//  so the Todos component should not re-render since neither the todos state nor the addTodo function are changing when the count is incremented.
+// This is because of something called "referential equality".
+// Every time a component re-renders, its functions get recreated. 
+// Because of this, the addTodo function has actually changed.
