ce03a51673f262305d8ee5dcad5fdbdc67f9c4ee
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-04 07:35:12 -0500

    Explanations

--- src/components/T15useReducer1.jsx
+++ src/components/T15useReducer1.jsx
@@ -1,9 +1,12 @@
 import { useReducer } from "react"
 
+// 1. Primer argumento del useReducer
 const initialState = () => ({
   count: 0,
 })
 
+// 2. Segundo argumento del useReducer
+// reducer recibe un STATE (objeto de javascript) y un ACTION (string)
 const reducer = (state = initialState(), action = {}) => {
   if(action.type === 'INCREMENT'){
     return { count: state.count + 1 }
@@ -11,14 +14,15 @@ const reducer = (state = initialState(), action = {}) => {
 }
 
 export default function T15useReducer1 (props) {
+  // STATE y DISPATCH es la manera local de denominar al INITIALSTATE(1) y a las funciones que define REDUCER(2)
   const [state, dispatch] = useReducer(reducer, initialState());
 
-  // Función local (plantilla o contenedora) para implementar REDUCER
+  // Función local (plantilla o contenedora) para implementar una determinada función de REDUCER
   const increment = () => dispatch({ type: 'INCREMENT'})
 
   return(
     <>
-      <h1>El nuevo valor es: {state}</h1>
+      <h2>El nuevo valor es: {state.count}</h2>
       <button onClick={increment}>Incrementar</button>
     </>
   )
